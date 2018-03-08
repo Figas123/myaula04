7af6be159ff3dc0ef814007193f5f888e5a0efcf
Nuno <nunosilvares@hotmail.com>
2018-03-08 12:33:17 +0000

    Adicionar o metodo CountToN novo e implementacao

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -20,27 +20,35 @@ namespace MyAula04
             }
         }
         /// <summary>
+        /// Meant to count incresingly between the numbers n1 and n2, inclusivly
+        /// </summary>
+        /// <param name="n1">Starts counting here</param>
+        /// <param name="n2">Stops counting here</param>
+        static void CountToN(int n1, int n2)
+        {
+            for(int i = n1; i <= n2; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        /// <summary>
         /// The body of the program, where the program starts and ends.
         /// </summary>
         /// <param name="args">Arguments used</param>
         static void Main(string[] args)
         {
             string s;
-            int nCount;
+            int n1, n2;
 
-            Console.Write("Ate' que valor queres na 1º contagem? ");
+            Console.Write("Qual o valor em que queres comecar a contagem? ");
             s = Console.ReadLine();
-            nCount = Convert.ToInt32(s);
-            Console.WriteLine("\n1º Contagem\n-------");
-            CountToN(nCount);
+            n1 = Convert.ToInt32(s);
+            Console.Write("Qual o valor em que queres parar de contagem? ");
+            s = Console.ReadLine();
+            n2 = Convert.ToInt32(s);
+            Console.WriteLine("\nContagem\n-------");
+            CountToN(n1, n2);
             Console.WriteLine("-------");
-
-            Console.Write("Ate' que valor queres na 2º contagem? ");
-            s = Console.ReadLine();
-            nCount = Convert.ToInt32(s);
-            Console.WriteLine("\n2º Contagem\n-------");
-            CountToN(nCount);
-            Console.WriteLine("\n");
             Console.ReadKey();
         }
     }
